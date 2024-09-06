d1d888fc9ca2fc792fb664b5c9fedc75dd54447c
Jhansi Lakshmi <118728615+jhansi9933@users.noreply.github.com>
2024-09-06 13:02:15 +0530

    Add files via upload

---
+++ CODEALPHA-main/PORTFOLIO/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer"], // Corrected 'Strings' to 'strings'
+    typeSpeed: 100,
+    backSpeed: 100, // Corrected 'backspeed' to 'backSpeed'
+    backDelay: 1000,
+    loop: true
+});
